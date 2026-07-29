5c1b25a88ed6ef3500989b25011f2fc8defce2ff
CarlosGRVM <carlos9.cm9@gmail.com>
2026-07-29 11:52:45 -0600

    API final y lista para su uso

--- mezcalapi/urls.py
+++ mezcalapi/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -33,4 +33,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/registro/', RegistroView.as_view(), name='registro'),
     path('api/me/', me_view, name='me'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
